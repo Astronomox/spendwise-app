8ff224072afb377deab8ffcf598de5b179a2c36c
Oluwasemilore Odusola <odusolasemilore@gmail.com>
2026-04-17 10:55:14 +0100

    remove sensitive seed and test files from tracking

--- backend/test.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const token = jwt.sign(
-    { id: "3f289b2b-a659-4295-a3a8-85412d4685dd" },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" }
-);
-
-console.log(token);
